73c1c559450c2e54d090e52c2957071ebe1dddac
MeezaanD <meezaandavids365@gmail.com>
2024-01-29 23:54:20 +0200

    feat: rollback on cors access

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const config = require('./config/config');
 const routes = require('./routes');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: '*' }));
 
 // Include your routes and configurations
 routes.startServer(app, config);
