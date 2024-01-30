9201a4411ad91f53865b2994bf32662b0bbb4533
MeezaanD <meezaandavids365@gmail.com>
2024-01-31 00:15:38 +0200

    bug: small fix for backend deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const connection = require('./config');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); /
+app.use(cors()); 
 app.use(express.json());
 
 app.get('/workshops', (req, res) => {
