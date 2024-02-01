35b76e5ce23987d5603a54c920142cf14acf26fd
MeezaanD <meezaandavids365@gmail.com>
2024-02-01 11:47:21 +0200

    feat: added dropdown filter to landing page

--- sharenet/src/views/ContactPageView.vue
+++ sharenet/src/views/ContactPageView.vue
@@ -4,13 +4,13 @@
 			<h2>Contact Us</h2>
 			<router-link to="/"><i class="bi bi-arrow-left"></i> Return Home</router-link>
 			<div class="row pt-5 text-center">
-				<div class="col-lg-5 col-md-6 col-sm-12 pt-lg-5">
+				<div class="col-lg-6 col-md-6 col-sm-12 pt-lg-5">
 					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.4949556038996!2d18.627532676377612!3d-33.87690447322349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc41e6218f7ab3%3A0xbdd802ca53b92d43!2sSharenet%20(Pty)%20Ltd!5e0!3m2!1sen!2sza!4v1706621523182!5m2!1sen!2sza"
-						width="500" height="500" style="border: 0" loading="lazy"
+						width="700" height="500" style="border: 0" loading="lazy"
 						referrerpolicy="no-referrer-when-downgrade"></iframe>
 				</div>
-				<div class="col-lg-7 col-md-6 col-sm-12 text-start pt-5">
+				<div class="col-lg-4 col-md-6 col-sm-12 text-start pt-5">
 					<div class="info p-4 rounded-3 bg-body-tertiary">
 						<h2>Contact Information</h2>
 						<h3>FIND US <span><a target="_blank" href="https://www.sharenet.co.za/">HERE</a></span></h3>
--- sharenet/src/views/LandingPageView.vue
+++ sharenet/src/views/LandingPageView.vue
@@ -1,57 +1,114 @@
 <template>
-	<section id="landing">
-		<div class="container-fluid py-3">
-			<h2>Sharenet Technical Assessment</h2>
-			<div class="d-flex gap-1">
-				<div v-show="showSearch" class="mt-2 w-100">
-					<input v-model="searchTerm" type="text" class="form-control search w-100"
-						placeholder="Search by Full Name" />
-				</div>
-				<button class="btn btn-primary" @click="toggleSearch">
-					{{ showSearch ? "Cancel" : "Search" }}
-				</button>
-			</div>
-			<div class="table-responsive bg-transparent table-wrapper pt-3">
-				<table class="table table-hover bg-transparent table-bordered caption-top text-light">
-					<caption class="text-dark">List of Stocks</caption>
-					<thead>
-						<tr>
-							<th scope="col">Full Name</th>
-							<th scope="col">
-								<button class="btn btn-outline-dark" @click="sortByPrice">
-									Price <i class="bi bi-arrow-down-up"></i>
-								</button>
-							</th>
-							<th scope="col">
-								<button class="btn btn-outline-dark" @click="sortByMove">
-									Move <i class="bi bi-arrow-down-up"></i>
-								</button>
-							</th>
-							<th scope="col">
-								<button class="btn btn-outline-dark" @click="sortByPmove">
-									Percentage Move <i class="bi bi-arrow-down-up"></i>
-								</button>
-							</th>
-							<th scope="col">
-								<button class="btn btn-outline-dark" @click="sortByDatetime">
-									Time <i class="bi bi-arrow-down-up"></i>
-								</button>
-							</th>
-						</tr>
-					</thead>
-					<tbody class="text-dark">
-						<tr v-for="(spot, index) in filteredSpotPrices" :key="index">
-							<td>{{ spot.fullName }}</td>
-							<td>{{ spot.price.toFixed(2) }}</td>
-							<td>{{ spot.move }}</td>
-							<td>{{ spot.pmove.toFixed(2) }}%</td>
-							<td>{{ spot.datetime }}</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</section>
+  <section id="landing">
+    <div class="container-fluid py-3">
+      <h2>Sharenet Technical Assessment</h2>
+      <div class="d-flex align-items-center gap-1">
+        <label
+          for="categoryFilter"
+          class="form-label pt-4"
+        >Category:</label>
+        <select
+          id="categoryFilter"
+          v-model="selectedCategory"
+          class="form-select w-25 mt-3"
+        >
+          <option value="all">All</option>
+          <option
+            v-for="category in uniqueCategories"
+            :key="category"
+            :value="category"
+          >
+            {{ category }}
+          </option>
+        </select>
+        <label
+          for="showOption"
+          class="form-label pt-4"
+        >Show:</label>
+        <select
+          id="showOption"
+          v-model="showOption"
+          class="form-select w-25 mt-3"
+        >
+          <option value="all">All</option>
+          <option value="latest5">Latest 5</option>
+        </select>
+        <div
+          v-show="showSearch"
+          class="mt-3 w-100"
+        >
+          <input
+            v-model="searchTerm"
+            type="text"
+            class="form-control search w-100"
+            placeholder="Search by Full Name"
+          />
+        </div>
+        <button
+          class="btn btn-primary mt-3"
+          @click="toggleSearch"
+        >
+          {{ showSearch ? "Cancel" : "Search" }}
+        </button>
+      </div>
+      <div class="table-responsive bg-transparent table-wrapper pt-3">
+        <table class="table table-hover bg-transparent table-bordered caption-top text-light">
+          <caption class="text-dark">
+            List of Stocks
+          </caption>
+          <thead>
+            <tr>
+              <th scope="col">Full Name</th>
+              <th scope="col">
+                <button
+                  class="btn btn-outline-dark"
+                  @click="sortByPrice"
+                >
+                  Price <i class="bi bi-arrow-down-up"></i>
+                </button>
+              </th>
+              <th scope="col">
+                <button
+                  class="btn btn-outline-dark"
+                  @click="sortByMove"
+                >
+                  Move <i class="bi bi-arrow-down-up"></i>
+                </button>
+              </th>
+              <th scope="col">
+                <button
+                  class="btn btn-outline-dark"
+                  @click="sortByPmove"
+                >
+                  Percentage Move <i class="bi bi-arrow-down-up"></i>
+                </button>
+              </th>
+              <th scope="col">
+                <button
+                  class="btn btn-outline-dark"
+                  @click="sortByDatetime"
+                >
+                  Time <i class="bi bi-arrow-down-up"></i>
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="text-dark">
+            <tr
+              v-for="(spot, index) in filteredSpotPrices"
+              :key="index"
+            >
+              <td>{{ spot.fullName }}</td>
+              <td>{{ spot.price.toFixed(2) }}</td>
+              <td>{{ spot.move }}</td>
+              <td>{{ spot.pmove.toFixed(2) }}%</td>
+              <td>{{ spot.datetime }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -59,87 +116,113 @@ import { defineComponent } from "vue";
 import axios from "axios";
 
 interface Spot {
-	code: string;
-	fullName: string;
-	price: number;
-	move: number;
-	pmove: number;
-	tickerId: number;
-	datetime: string;
+  code: string;
+  fullName: string;
+  price: number;
+  move: number;
+  pmove: number;
+  tickerId: number;
+  datetime: string;
+  categoryName: string;
 }
 
 export default defineComponent({
-	name: "landing",
-	data() {
-		return {
-			spotPrices: [] as Spot[],
-			showSearch: false,
-			searchTerm: "",
-			sortPriceAscending: true,
-			sortPmoveAscending: true,
-			sortMoveAscending: true,
-			sortDatetimeAscending: true,
-		};
-	},
-	mounted() {
-		this.fetchSpotPrices();
-	},
-	computed: {
-		filteredSpotPrices(): Spot[] {
-			if (!this.searchTerm.trim()) {
-				return this.spotPrices;
-			}
+  name: "landing",
+  data() {
+    return {
+      spotPrices: [] as Spot[],
+      showSearch: false,
+      searchTerm: "",
+      sortPriceAscending: true,
+      sortPmoveAscending: true,
+      sortMoveAscending: true,
+      sortDatetimeAscending: true,
+      selectedCategory: "all",
+      showOption: "latest5",
+    };
+  },
+  mounted() {
+    this.fetchSpotPrices();
+  },
+  computed: {
+    filteredSpotPrices(): Spot[] {
+      let filteredSpotPrices = this.spotPrices;
 
-			const term = this.searchTerm.toLowerCase().trim();
-			return this.spotPrices.filter((spot) =>
-				spot.fullName.toLowerCase().includes(term)
-			);
-		},
-	},
-	methods: {
-		async fetchSpotPrices() {
-			try {
-				const response = await axios.get(
-					"https://api.sharenet.co.za/api/v1/px2/spots"
-				);
-				this.spotPrices = response.data.spots;
-				console.log(response);
-			} catch (error) {
-				console.error("Error fetching spot prices:", error);
-			}
-		},
-		toggleSearch() {
-			this.showSearch = !this.showSearch;
-			this.searchTerm = "";
-		},
-		sortByPrice() {
-			this.sortPriceAscending = !this.sortPriceAscending;
-			const direction = this.sortPriceAscending ? 1 : -1;
-			this.spotPrices.sort((a, b) => direction * (a.price - b.price));
-		},
-		sortByPmove() {
-			this.sortPmoveAscending = !this.sortPmoveAscending;
-			const direction = this.sortPmoveAscending ? 1 : -1;
-			this.spotPrices.sort((a, b) => direction * (a.pmove - b.pmove));
-		},
-		sortByMove() {
-			this.sortMoveAscending = !this.sortMoveAscending;
-			const direction = this.sortMoveAscending ? 1 : -1;
-			this.spotPrices.sort((a, b) => direction * (a.move - b.move));
-		},
-		sortByDatetime() {
-			this.sortDatetimeAscending = !this.sortDatetimeAscending;
-			const direction = this.sortDatetimeAscending ? 1 : -1;
-			this.spotPrices.sort(
-				(a, b) =>
-					direction *
-					(new Date(a.datetime).getTime() - new Date(b.datetime).getTime())
-			);
-		},
-	},
+      if (this.selectedCategory !== "all") {
+        filteredSpotPrices = filteredSpotPrices.filter(
+          (spot) => spot.categoryName === this.selectedCategory
+        );
+      }
+
+      if (this.showOption === "latest5") {
+        filteredSpotPrices = filteredSpotPrices
+          .sort(
+            (a, b) =>
+              new Date(b.datetime).getTime() - new Date(a.datetime).getTime()
+          )
+          .slice(0, 5);
+      }
+
+      if (this.searchTerm.trim() !== "") {
+        const term = this.searchTerm.toLowerCase().trim();
+        filteredSpotPrices = filteredSpotPrices.filter(
+          (spot) => spot.fullName.toLowerCase().includes(term)
+        );
+      }
+
+      return filteredSpotPrices;
+    },
+    uniqueCategories(): string[] {
+      const uniqueCategoriesSet = new Set<string>();
+      this.spotPrices.forEach((spot) =>
+        uniqueCategoriesSet.add(spot.categoryName)
+      );
+      return Array.from(uniqueCategoriesSet);
+    },
+  },
+  methods: {
+    async fetchSpotPrices() {
+      try {
+        const response = await axios.get(
+          "https://api.sharenet.co.za/api/v1/px2/spots"
+        );
+        this.spotPrices = response.data.spots;
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching spot prices:", error);
+      }
+    },
+    toggleSearch() {
+      this.showSearch = !this.showSearch;
+      this.searchTerm = "";
+    },
+    sortByPrice() {
+      this.sortPriceAscending = !this.sortPriceAscending;
+      const direction = this.sortPriceAscending ? 1 : -1;
+      this.spotPrices.sort((a, b) => direction * (a.price - b.price));
+    },
+    sortByPmove() {
+      this.sortPmoveAscending = !this.sortPmoveAscending;
+      const direction = this.sortPmoveAscending ? 1 : -1;
+      this.spotPrices.sort((a, b) => direction * (a.pmove - b.pmove));
+    },
+    sortByMove() {
+      this.sortMoveAscending = !this.sortMoveAscending;
+      const direction = this.sortMoveAscending ? 1 : -1;
+      this.spotPrices.sort((a, b) => direction * (a.move - b.move));
+    },
+    sortByDatetime() {
+      this.sortDatetimeAscending = !this.sortDatetimeAscending;
+      const direction = this.sortDatetimeAscending ? 1 : -1;
+      this.spotPrices.sort(
+        (a, b) =>
+          new Date(a.datetime).getTime() - new Date(b.datetime).getTime()
+      );
+    },
+  },
 });
 </script>
 
 <style>
-@import "../assets/css/style.css"
+@import "../assets/css/style.css";
 </style>
